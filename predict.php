fa0a7d4b8cee3e12bc9fa7e496ff892aa8b3b449
ChristopherWilliam6181901048 <christ.william.bill@gmail.com>
2022-01-01 14:15:02 +0700

    Menambah tampilan

--- predict.php
+++ predict.php
@@ -2,13 +2,15 @@
 <html>
 <head>
     <link rel="stylesheet" href="styles.css">
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+
 <title>Predict Page</title>
 
 </head>
 <body>
     <div class="NavBar">
 
-        <h1 class="weather">Weather</h1>
+        <h1 class="weather"><i class='fas fa-cloud-sun'></i>  Predict Weather</h1>
 
         <div class="Buttons">
             <a class="btn" href="HomePage.php">Home</a>
@@ -20,7 +22,7 @@
     <div class="predictContent">
         <div class="predictInput">
             <h2>
-                Input data to predict weather: 
+                Input data to predict the weather: 
             </h2>
             <form action="PredictController.php" method="get" class="formPredict">
 
@@ -46,27 +48,31 @@
                         echo "
                         <h3>Weather for Today:</h3>
                         <h1>Raining</h1>
+                        <i class='fas fa-cloud-showers-heavy weatherIcon'></i>
                         ";
                     }else if($_GET['resultToday'] == 0){
                         echo "
                         <h3>Weather for Today:</h3>
                         <h1>Sunny</h1>
+                        <i class='fas fa-sun weatherIcon'></i>
                         ";                    
                     }
                 }
 
-                echo "<br>";
+                echo "<br><br><br><br><br>";
 
                 if(isset($_GET['resultTomorrow'])){
                     if($_GET['resultTomorrow'] == 1){
                         echo "
                         <h3>Weather for Tomorrow:</h3>
                         <h1>Raining</h1>
+                        <i class='fas fa-cloud-showers-heavy weatherIcon'></i>
                         ";
                     }else if($_GET['resultTomorrow'] == 0){
                         echo "
                         <h3>Weather for Tomorrow:</h3>
                         <h1>Sunny</h1>
+                        <i class='fas fa-sun weatherIcon'></i>
                         ";                    
                     }
                 }
